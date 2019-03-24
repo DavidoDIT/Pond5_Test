ca4cf0913b0d5cbd0b41033c7a3469c1ca0bfba5
David O'Neill <doneill39@gmail.com>
2019-03-24 15:54:39 +0000

    added '/ping' and '/sytem'

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+import platform
+import requests
+
+
+app = Flask(__name__)
+
+page = requests.get("https://www.pond5.com/photo/11497188/")
+print(page.status_code)  # Checking for a 200 response
+
+pic_soup = BeautifulSoup(page.text, 'html.parser')
+type(pic_soup)
+
+
+@app.route('/')
+def test():
+    test = "Test"
+    return test
+
+@app.route('/ping')
+def ping():
+    if page.status_code == 200:
+        return "PONG!"
+    else:
+        return "Error"
+
+@app.route('/system')
+def system():
+    info = {"version": platform.uname().version,
+            "system": platform.uname().system,
+            "processor": platform.processor(),
+            "architecture": platform.architecture()}
+    return jsonify(info)
+
+
+@app.route('/mediainfo')
+def pic():
+    return pic
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
 
 
 
