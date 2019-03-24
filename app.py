d35ca3956efa9b0c15debf07160eb91e006c040e
David O'Neill <doneill39@gmail.com>
2019-03-24 17:26:11 +0000

    finished main test

--- app.py
+++ app.py
@@ -3,45 +3,62 @@ from bs4 import BeautifulSoup
 import platform
 import requests
 
-
 app = Flask(__name__)
 
-page = requests.get("https://www.pond5.com/photo/11497188/")
-print(page.status_code)  # Checking for a 200 response
 
-pic_soup = BeautifulSoup(page.text, 'html.parser')
-type(pic_soup)
-
-
-@app.route('/')
+@app.route("/")
 def test():
     test = "Test"
     return test
 
-@app.route('/ping')
+
+@app.route("/ping", methods=["GET"])
 def ping():
-    if page.status_code == 200:
+    pond = requests.get("https://www.pond5.com/photo/11497188")
+    if pond.status_code == 200:
         return "PONG!"
     else:
         return "Error"
 
-@app.route('/system')
+
+@app.route("/system")
 def system():
-    info = {"version": platform.uname().version,
-            "system": platform.uname().system,
-            "processor": platform.processor(),
-            "architecture": platform.architecture()}
+    info = {
+        "version": platform.uname().version,
+        "system": platform.uname().system,
+        "processor": platform.processor(),
+        "architecture": platform.architecture(),
+    }
     return jsonify(info)
 
 
-@app.route('/mediainfo')
-def pic():
-    return pic
+@app.route("/mediainfo/<media_id>", methods=["GET"])
+def media(media_id):
+    page = requests.get("https://www.pond5.com/photo/" + str(media_id))
+    print(page.status_code)  # Checking for a 200 response
+    soup = BeautifulSoup(page.text, "html.parser")
+    filename = soup.find("meta", property="og:image")
+    filename = filename["content"]
+    height = soup.find("meta", property="og:image:height")
+    height = height["content"]
+    width = soup.find("meta", property="og:image:width")
+    width = width["content"]
+    title = soup.find("meta", property="twitter:title")
+    title = title["content"]
+    type = soup.find("meta", property="og:image:type")
+    type = type["content"]
+    size = soup.find_all("dd")[12].text
+
+    pic_info = {
+        "filename": filename,
+        "size": size,
+        "height": height,
+        "width": width,
+        "title": title,
+    }
+
+    return jsonify(pic_info)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
-
-
-
-
